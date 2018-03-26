b4681630be7cf901c939955355ed22bcfc873ad9
mdziedzi <marcindziedzic8@gmail.com>
2018-03-26 21:24:07 +0200

    Doc for task-3 completed

--- project-1/task-3/md_gauss_seidel_for_plot.m
+++ project-1/task-3/md_gauss_seidel_for_plot.m
@@ -1,4 +1,4 @@
-function [r, iter] = md_gauss_seidel (A, e)
+function [r, iter] = md_gauss_seidel_for_plot (A, e)
 
     % sekcja inicjalizacyjna
     w = size(A,1);
@@ -8,6 +8,42 @@ function [r, iter] = md_gauss_seidel (A, e)
     b = A(:,k);
     A = A(:, 1:k-1);
     iter = 1;
+    
+    L = zeros(w,w);
+D = zeros(w,w);
+U = zeros(w,w);
+
+    
+    %Sprawdzenie warunku dostatecznego zbieznosci - silnej dominacji
+    %diagonalnej macierzy A
+    for i = 1:w
+        %Sumujemy wszystkie elementy w wierszu, poza diagonalnym
+        sum = 0;
+        for j = 1:w    
+            if i~=j
+                sum = sum + abs(A(i,j));
+            end
+        end
+        %Jesli suma jest wieksza od elementu to warunek nie jest spelniony
+        if sum > abs(A(i,i))
+            disp('Warunek silnej dominacji diagonalnej nie jest spelniony');
+        return
+        end
+    end
+ 
+%Stworzenie macierzy trojkatnych oraz diagonalnej
+for i = 1:w
+    for j = 1:w
+        if(i<j)
+            U(i,j) = A(i,j);
+        elseif(i>j)
+            L(i,j) = A(i,j);
+        else
+            D(i,j) = A(i,j);
+        end
+    end
+end
+
     
     % petla glowna
     while(r>e)
--- project-1/task-3/plots_for_task_3.m
+++ project-1/task-3/plots_for_task_3.m
@@ -2,8 +2,8 @@
 z =[];
 x = [];
 e = 0.1;
-for i = 1:150
-    [z(i),x(i)] = md_gauss_seidel(md_prepare_data_d, e);
+for i = 1:10
+    [z(i),x(i)] = md_gauss_seidel_for_plot(md_prepare_data_a(1280), e);
     e = e/10;
 end
 plot(x,z);
