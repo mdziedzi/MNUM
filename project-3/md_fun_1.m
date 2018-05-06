648850666c1797084244bbad820223026888cef7
mdziedzi <marcindziedzic8@gmail.com>
2018-05-06 22:40:53 +0200

    Add Newton function

--- project-3/md_fun_1.m
+++ project-3/md_fun_1.m
@@ -1,8 +1,10 @@
 %funkcja matemtyczna podana w zadaniu 1
-function y = md_fun_1 (x)
+function [y, dy] = md_fun_1 (x)
   size1=size(x,2);
   y=zeros(1,size1);
+  dy=zeros(1,size1);
   for i=1:size1
     y(1,i)=0.7*x(1,i)*cos(x(1,i))-log(x(1,i)+1);
+    dy(1,i)=-1/(1 + x(1,i) + 0.7 * cos(x(1,i)) - 0.7 * x * sin(x(1,i)));
   end
 end
---
+++ project-3/md_newton.m
@@ -0,0 +1,27 @@
+% Funkcja wyznaczajaca punkty zerowe funkcji metoda Newtona
+%
+% IN:
+% a0 - lewa strona zakresu
+% fun - funkcja 
+% iter - maksymalna liczba iteracji
+%
+% OUT:
+% solution - wyznaczone miejsce zerowe
+function solution = md_newton(fun, a0,iter)
+  x0 = a0; 
+  for k = 1:iter
+    [fold, fpold] = feval(fun,x0); 
+    dx = fold / fpold; 
+    x0 = x0 - dx;
+    fprintf('%3d	%12.10f     %12.16f     %12.3e \n',k,dx,x0,fold);
+    if(fold == 0)
+        return
+    end
+    %dodatkowy warunek zatrzumania
+	if fold==0 
+        solution = x0;
+        break; 
+    end
+  end
+end
+  
