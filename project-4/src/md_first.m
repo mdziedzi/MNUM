4a619eecd5f929ddbc3fcf19c8d05b4147cabc7c
mdziedzi <marcindziedzic8@gmail.com>
2018-06-10 12:09:30 +0200

    First task almoust done

--- project-4/src/md_first.m
+++ project-4/src/md_first.m
@@ -1,33 +1,15 @@
-%Realizacja podpunktu 1 metoda RK4 ze stalym krokiem
+% Realizacja zadania 1 
+% metoda RK4 ze stalym krokiem
 clear; 
 zero=[8 7; 0 0.4; 5 0; 0.01 0.001]; %wektor stanow poczatkowych
-%  zero=[0.00001 0.2; 10 0.4; 5 0; 0.01 0.001]; %wektor stanow poczatkowych
-
 step = 0.0004; %krok
 
-for k = 1:1
-    
+for k = 3:3
     data = md_rk4s(zero(k,:),20,step);
-    
-%     error = md_rk4s(zero(k,:),20,step/2);
-%     for i=1:(20/step)
-%         error(i,1:2) = abs(data(i,1:2)-error(2*i,1:2));
-%     end
-%     n = norm(error);
-%     disp(n); 
-    
-    h = figure;%('visible','off');
+    h = figure;
     plot(data(:,1),data(:,2),'-o');
     l = size(data,1);
     hold on;
-    xl = get(gca,'xlim');
-    yl = get(gca,'ylim');
-    zl = get(gca,'zlim');
-    
-%     plot3(data(:,1),data(:,2),repmat(zl(1),l,1),'-');
-%     plot3(data(:,1),repmat(yl(2),l,1),data(:,3),'-');
-%     plot3(repmat(xl(2),l,1),data(:,2),data(:,3),'-');
-    
     grid on;
     name =  ['metoda RK4 krok:' num2str(step) ' podpunkt:' num2str(k)]; 
     title(name);
--- project-4/src/md_fx.m
+++ project-4/src/md_fx.m
@@ -1,8 +1,6 @@
 function [D] = md_fx(x)
 % Funkcja zwracająca pochodne  
-
 D(1) = x(2) + x(1)*(0.2 -x(1)^2 -x(2)^2); 
 D(2) = -x(1) + x(2)*(0.2 -x(1)^2 -x(2)^2);
-
 end
 
